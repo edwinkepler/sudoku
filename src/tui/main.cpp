21f91a27351d33315d218ef602093ac24c7b7601
edwinkepler <edwinkepler@protonmail.com>
2017-11-19 00:23:31 +0100

    Add print_sudoku_std() function

--- src/tui/main.cpp
+++ src/tui/main.cpp
@@ -7,15 +7,34 @@
 
 #include "game/sudoku.hpp"
 
-int main() {
-    auto v_puzzle = Sudoku::create_std();
+void print_sudoku_std(std::vector<std::vector<short>> _puzzle) {
+    std::cout << "  ABC DEF GHI" << std::endl;
     for(int i = 0; i < 9; i++) {
-        for(const auto& j : v_puzzle[i]) {
-            std::cout << j;
+        if(i == 3 || i == 6) {
+            std::cout << "  ";
+            for(int k = 0; k < 11; k++) {
+                if(k == 3 || k == 7) {
+                    std::cout << "|";
+                } else {
+                    std::cout << "-";
+                }
+            }
+            std::cout << std::endl;
+        }
+        std::cout << i + 1 << " ";
+        for(int j = 0; j < 9; j++) {
+            if(j == 3 || j == 6) {
+                std::cout << "|";
+            }
+            std::cout << _puzzle[i][j];
         }
         std::cout << std::endl;
     }
-
     std::cout << std::endl;
+}
+
+int main() {
+    auto v_puzzle = Sudoku::create_std();
+    print_sudoku_std(v_puzzle);
     return 0;
 }
